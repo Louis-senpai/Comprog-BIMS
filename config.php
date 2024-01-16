b1eab1313a2d2c50f64f4f2b924b17a023443b70
ゆか（Yuka）San <128908897+KazuYuika@users.noreply.github.com>
2024-01-17 07:18:12 +0800

    changed access

--- config.php
+++ config.php
@@ -1,39 +1,39 @@
 <?php 
 define('ROOT_DIR', realpath(__DIR__));
-// $host = "aws.connect.psdb.cloud";
-// $user = "czm65xx2cd3zqlrfqrnh";
-// $name = "student";
-// $pass = "pscale_pw_lQDV8n86R55Ul3CjYFbzT6m5BkochiFY5kyz8z2Z2ti";
-// $port = "3306";
-// $ca_path = 'cacert-2023-12-12.pem';
+$host = "aws.connect.psdb.cloud";
+$user = "nrnv4k1rwfutfd9y7fbj";
+$name = "student";
+$pass = "pscale_pw_aAwyZ9a9JaNWB3sLRolLqQTAVIPUgsJRvyrddL9or8C";
+$port = "3306";
+$ca_path = 'cacert-2023-12-12.pem';
 
 
 
-// $conn = mysqli_init();
-// if (!$conn) {
-//     die("mysqli_init failed");
-// }
+$conn = mysqli_init();
+if (!$conn) {
+    die("mysqli_init failed");
+}
 
-// // Set SSL options
-// mysqli_ssl_set($conn, NULL, NULL, $ca_path, NULL, NULL);
+// Set SSL options
+mysqli_ssl_set($conn, NULL, NULL, $ca_path, NULL, NULL);
 
-// // Establish the connection
-// if (!mysqli_real_connect($conn, $host, $user, $pass, $name, NULL, NULL, MYSQLI_CLIENT_SSL)) {
-//     die("Connection failed: " . mysqli_connect_error());
+// Establish the connection
+if (!mysqli_real_connect($conn, $host, $user, $pass, $name, NULL, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 
-// }
+}
 // else {
 //     echo "Connection to database successful";
 // }
 
 
-$host = "delta.optiklink.com";
-$user = "u126067_6YObtWKgqn";
-$name = "s126067_student";
-$pass = "!yQ4Q2@Da6BB!8VWIhLcKKMw";
-$port = "3306";
+// $host = "delta.optiklink.com";
+// $user = "u126067_6YObtWKgqn";
+// $name = "s126067_student";
+// $pass = "!yQ4Q2@Da6BB!8VWIhLcKKMw";
+// $port = "3306";
 
-$conn = mysqli_connect($host, $user, $pass, $name, $port);
+// $conn = mysqli_connect($host, $user, $pass, $name, $port);
 
 
 
--- includes/components/nav.php
+++ includes/components/nav.php
@@ -1,4 +1,4 @@
-<nav class="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+<nav class="fixed z-30 w-full bg-lime-100 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
             <div class="flex items-center justify-start">
@@ -18,7 +18,7 @@
                     </svg>
                 </button>
                 <a href="https://flowbite-admin-dashboard.vercel.app/" class="flex ml-2 md:mr-24">
-                    <img src="https://flowbite-admin-dashboard.vercel.app/images/logo.svg" class="h-8 mr-3"
+                    <img src="../includes/images/logo1.png" class="h-8 mr-3"
                         alt="FlowBite Logo">
                     <span
                         class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Barangay
