019ab80dcd9926f22b99e6fc6dbe576e53048228
Zedrick <zedrickpiolocastro@gmail.com>
2024-01-17 17:26:12 +0800

    addaccountformupdate

--- admin/addAccount.php
+++ admin/addAccount.php
@@ -15,7 +15,7 @@ require_once '../includes/components/header.php';
                 <div class="px-4 pt-6 pr-4">
                     
             <?php 
-            require_once '../signup.php';
+            require_once '../includes/components/addaccountform.php';
             ?>
 
                 </div>
--- includes/components/addaccountform.php
+++ includes/components/addaccountform.php
@@ -1,10 +1,6 @@
 <section class="">
                 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <a href="index.php" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                        <img class="w-8 h-8 mr-2" src="includes/images/logo1.png"
-                            alt="logo">
-                        BIMS
-                    </a>
+                    
                     <div
                         class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -45,13 +41,9 @@
                                 </div>
                                 
                                 <button type="submit" name="create_account"
-                                    class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign
-                                    Up</button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Already have An Account? <a href="#"
-                                        class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign
-                                        in</a>
-                                </p>
+                                    class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Add Account</button>
+                                
                             </form>
                         </div>
                     </div>
