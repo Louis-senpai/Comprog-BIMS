945e7c4927ff1379190fe2e8d9f4cbd3971f0a0f
angel@com <angel@com>
2024-01-21 17:44:33 +0800

    ...

--- admin/display_officers.php
+++ admin/display_officers.php
@@ -3,9 +3,10 @@ require_once '../config.php';
 
 $result = $conn->query("SELECT * FROM officers");
 while ($row = $result->fetch_assoc()) {
-  echo '<tr style="border: 1px solid #E5E7EB; text-align: center;">
-            <td>' . $row['name'] . '</td>
-            <td>' . $row['position'] . '</td>
+  echo '<tr align="center">
+            <td class="p-2 border border-gray-300">' . $row['name'] . '</td>
+            <td class="p-2 border border-gray-300">' . $row['position'] . '</td>
+            <td class="p-2 border border-gray-300">' . $row['phone_number'] . '</td>
           </tr>';
 
 
